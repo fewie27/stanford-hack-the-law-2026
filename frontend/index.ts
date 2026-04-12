fdf37df9cf15e32861ed3fe18386248861c28f3d
dora <doraganx@gmail.com>
2026-04-12 15:07:08 -0700

    Add logo to home page

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,6 +1,6 @@
 import { registerRootComponent } from 'expo';
 
-import App from './App';
+import App from './App.version2';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
