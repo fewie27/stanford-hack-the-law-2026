99ebcfe02bc5e3e6166645367694e9c56dde8071
Felix Wiegand <felix.wiegand@neerby.de>
2026-04-12 15:19:08 -0700

    Updated modify

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,6 +1,6 @@
 import { registerRootComponent } from 'expo';
 
-import App from './App.version2';
+import App from './App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
